6be1bfb7059e7289e131d1950d1074af93dabbd8
Mario <mario.aguilar@planetmedia.com.mx>
2016-05-02 17:42:31 -0500

    toolbar terminada

--- app/src/main/java/com/example/planetmedia/demoappfinmex/MainActivity.java
+++ app/src/main/java/com/example/planetmedia/demoappfinmex/MainActivity.java
@@ -2,15 +2,21 @@ package com.example.planetmedia.demoappfinmex;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar tb = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(tb);
+
     }
 
     @Override
